a7f20bb2dd047232e9dc18455ca0a928549a705f
darrahts <31658834+darrahts@users.noreply.github.com>
2017-11-25 08:49:45 -0600

    Update dataAssignment.py

--- dataAssignment.py
+++ dataAssignment.py
@@ -2,9 +2,12 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-from sklearn import datasets, linear_model, preprocessing, cross_validation, svm
+import matplotlib.pyplot as plt
+from sklearn import tree, metrics, datasets, linear_model, preprocessing, cross_validation, svm
+from pylab import rcParams
 import os
 import csv
+import graphviz
 
 def LoadData(path, returnBunch=True):
     with open(os.path.join(path, 'BettysBrainDataForAnalysis.csv')) as f:
@@ -32,6 +35,34 @@ def LoadData(path, returnBunch=True):
 
     return datasets.base.Bunch(data=data, target=targets,feature_names=featureNames, DESCR="test description of the bettys brain  data")
 
+def LoadData1(path, returnBunch=True):
+    with open(os.path.join(path, 'BettysBrainDataForAnalysis.csv')) as f:
+        file = csv.reader(f)
+
+        #get first line
+        temp = next(file)
+        numSamples = int(temp[0])
+        numFeatures = int(temp[1])
+        
+        #generate empty arrays
+        data = np.empty((numSamples, numFeatures))
+        targets = np.empty((numSamples,))
+
+        #get next line which is the column headers, less the last column (-1) i.e. feature names
+        temp = next(file)
+        featureNames = np.array(temp[1:])
+
+        for i, d in enumerate(file):
+            data[i] = np.asarray(d[1:], dtype=np.float64) #all but last column are the data features
+            targets[i] = np.asarray(d[0], dtype=np.float64) #only the last column are the targets
+
+    if not returnBunch:
+        return data, targets
+
+    return datasets.base.Bunch(data=data, target=targets,feature_names=featureNames, DESCR="test description of the bettys brain  data")
+
+'''                                    REGRESSION                   '''
+# TO-DO polynomial regression, ridge regression, others
 
 def Test_1(df, target): # 91.6% prediction accuracy i.e. percentage of variance the model explains uses least squares
     '''These values are highly related to final map score using statsmodels.api.OLS(target, df).fit() '''
@@ -60,7 +91,6 @@ def Test_3(df, target):# 92.9%
     predictions = model.predict(X)
     print(model.summary())
 
-
 def Test_4(df, target): # 94.7% 
     '''This is the entire data set'''
     X = df
@@ -79,7 +109,6 @@ def Test_5(df, target): # 84.8%
         print(str(target["Final_Map_Score"][i]) + "\t\t" + str(predictions[i]))
     print("Score: " + str(model.score(df, target)))
 
-
 def Test_6(df, target):
     '''uses svr and the entire dataset. I dont really understand the confidence values given.'''
     X = preprocessing.scale(np.array(df))
@@ -92,31 +121,70 @@ def Test_6(df, target):
         confidence = classifier.score(testX, testY)
         print(type + "\t" + str(confidence))
 
+def RegressionTesting():
+    #load the data
+    data = LoadData("E:\Downloads")
+    #independent variables
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    #dependent variable
+    target = pd.DataFrame(data.target, columns=["Final_Map_Score"])
+
+    Test_1(df, target)
+    #Test_2(df, target)
+    #Test_3(df, target)
+    #Test_4(df, target)
+    #Test_5(df, target)
+    #Test_6(df, target)
+
+    #print(df["num_map_moves"])
+    #print(df)
+    #print(target["Final_Map_Score"][39])
+    #print(data.feature_names)
+    print(data.DESCR)
+
+
+'''                                    DECISION TREE                   '''
+
+
+def DecisionTreeTesting():
+    #load the data
+    data = LoadData1("E:\Downloads")
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    
+    X = df[["num_map_moves", "time_viewing_concept_map_ms", "num_causal_link_adds", "num_causal_link_adds_effective"]] 
+    #X = df[["num_map_moves", "time_viewing_concept_map_ms", "time_viewing_graded_questions_ms", "time_viewing_graded_explanations_ms", "time_viewing_ungraded_explanations_ms", "num_causal_link_adds", "num_causal_link_adds_effective"]]
+    #X = df
+
+    y = pd.DataFrame(data.target, columns=["subject_id"])
+    #split the data
+    xTrain, xTest, yTrain, yTest = cross_validation.train_test_split(X, y, test_size = .25, random_state=100)
+    #decision tree classifier
+    dtc_1 = tree.DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=5, min_samples_split=6, min_samples_leaf=3)
+    dtc_1.fit(xTrain, yTrain)
+    yPred_1 = dtc_1.predict(xTest)
+    print(yPred_1)
+    print(metrics.accuracy_score(yTest,yPred_1))
+
+    dtc_2 = tree.DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=5, min_samples_split=6, min_samples_leaf=3)
+    dtc_2.fit(xTrain, yTrain)
+    yPred_2 = dtc_2.predict(xTest)
+    print(metrics.accuracy_score(yTest,yPred_2))
+
+    #visualization - gives errors but should work
+    dot_data = tree.export_graphviz(dtc_1, out_file=None)
+    #graph = graphviz.Source(dot_data)
+    #graph.render("test")
+
+
+
 
 if __name__ == "__main__":
     #clear the warnings generated from the imported libraries
     os.system('cls' if os.name == 'nt' else 'clear')
+    DecisionTreeTesting()
 
-    #load the data
-    data = LoadData("C:\ProgramData\Anaconda3\Lib\site-packages\sklearn\datasets\data")
-    #print(data.feature_names)
-    #print(data.DESCR)
 
-    #independent variables
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    #print(df["num_map_moves"])
+    #print(X)
+    #print(Y)
 
-    #dependent variable
-    target = pd.DataFrame(data.target, columns=["Final_Map_Score"])
-    #print(target["Final_Map_Score"][39])
-    
-    #z = df[df.columns[2:4]]
-    #print(z)
 
-    #Test_1(df, target)
-    #Test_2(df, target)
-    #Test_3(df, target)
-    Test_4(df, target)
-    #Test_5(df, target)
-    #Test_6(df, target)
-    
