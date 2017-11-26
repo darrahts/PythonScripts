5e3b501d8ddece2e543d103a73b8899e28398a62
darrahts <timothy.s.darrah@vanderbilt.edu>
2017-11-26 12:47:21 -0600

    updates

--- dataAssignment.py
+++ dataAssignment.py
@@ -12,25 +12,26 @@ import pydotplus
 import collections
 
 def LoadData(path, returnBunch=True):
+    '''   This loads the data with map scores as targets  '''
     with open(os.path.join(path, 'BettysBrainDataForAnalysis.csv')) as f:
         file = csv.reader(f)
 
-        #get first line
+        #   get first line
         temp = next(file)
         numSamples = int(temp[0])
         numFeatures = int(temp[1])
         
-        #generate empty arrays
+        #   generate empty arrays
         data = np.empty((numSamples, numFeatures))
         targets = np.empty((numSamples,))
 
-        #get next line which is the column headers, less the last column (-1) i.e. feature names
+        #   get next line which is the column headers, less the last column (-1) i.e. feature names
         temp = next(file)
         featureNames = np.array(temp[:-1])
 
         for i, d in enumerate(file):
-            data[i] = np.asarray(d[:-1], dtype=np.float64) #all but last column are the data features
-            targets[i] = np.asarray(d[-1], dtype=np.float64) #only the last column are the targets
+            data[i] = np.asarray(d[:-1], dtype=np.float64) #   all but last column are the data features
+            targets[i] = np.asarray(d[-1], dtype=np.float64) #   only the last column are the targets
 
     if not returnBunch:
         return data, targets
@@ -38,25 +39,26 @@ def LoadData(path, returnBunch=True):
     return datasets.base.Bunch(data=data, target=targets,feature_names=featureNames, DESCR="test description of the bettys brain  data")
 
 def LoadData1(path, returnBunch=True):
+    ''' This loads the data with group/individual as target '''
     with open(os.path.join(path, 'BettysBrainDataForAnalysis.csv')) as f:
         file = csv.reader(f)
 
-        #get first line
+        #   get first line
         temp = next(file)
         numSamples = int(temp[0])
         numFeatures = int(temp[1])
         
-        #generate empty arrays
+        #   generate empty arrays
         data = np.empty((numSamples, numFeatures))
         targets = np.empty((numSamples,))
 
-        #get next line which is the column headers, less the last column (-1) i.e. feature names
+        #   get next line which is the column headers, less the last column (-1) i.e. feature names
         temp = next(file)
         featureNames = np.array(temp[1:])
 
         for i, d in enumerate(file):
-            data[i] = np.asarray(d[1:], dtype=np.float64) #all but last column are the data features
-            targets[i] = np.asarray(d[0], dtype=np.float64) #only the last column are the targets
+            data[i] = np.asarray(d[1:], dtype=np.float64) #   all but last column are the data features
+            targets[i] = np.asarray(d[0], dtype=np.float64) #   only the last column are the targets
 
     if not returnBunch:
         return data, targets
@@ -64,12 +66,28 @@ def LoadData1(path, returnBunch=True):
     return datasets.base.Bunch(data=data, target=targets,feature_names=featureNames, DESCR="test description of the bettys brain  data")
 
 
+def LoadData2(path):
+    ''' This loads the data set as a nxm matrix '''
+    with open(os.path.join(path, 'BettysBrainDataForAnalysis.csv')) as f:
+        file = csv.reader(f)
+        #   get first line
+        temp = next(file)
+        numSamples = int(temp[0])
+        numFeatures = int(temp[1])
+        
+        #   generate empty arrays
+        data = np.empty((numSamples, numFeatures))
+
+        for i, d in enumerate(file):
+            data[i] = np.asarray(d[:], dtype=np.float64) #
+    return data
+
 '''*****************************************************************'''
 '''                                    REGRESSION                   '''
 '''*****************************************************************'''
-# TO-DO polynomial regression, ridge regression, others
+#   TO-DO polynomial regression, ridge regression, others
 
-def Test_1(df, target): # 91.6% prediction accuracy i.e. percentage of variance the model explains uses least squares
+def Test_1(df, target): #   91.6% prediction accuracy i.e. percentage of variance the model explains uses least squares
     '''These values are highly related to final map score using statsmodels.api.OLS(target, df).fit() '''
 
     X = df[["num_map_moves", "time_viewing_concept_map_ms", "num_causal_link_adds", "num_causal_link_adds_effective"]]
@@ -78,7 +96,7 @@ def Test_1(df, target): # 91.6% prediction accuracy i.e. percentage of variance 
     predictions = model.predict(X)
     print(model.summary())
 
-def Test_2(df, target): # 69.5% prediction accuracy
+def Test_2(df, target): #   69.5% prediction accuracy
     '''These values are not so much related to final map score'''
 
     X = df[["total_system_time_ms", "time_viewing_graded_questions_ms", "time_viewing_notes_ms", "time_viewing_science_book_ms"]]
@@ -87,7 +105,7 @@ def Test_2(df, target): # 69.5% prediction accuracy
     predictions = model.predict(X)
     print(model.summary())
 
-def Test_3(df, target):# 92.9%
+def Test_3(df, target):#   92.9%
     '''These values are another combination'''
 
     X = df[["num_map_moves", "time_viewing_concept_map_ms", "time_viewing_graded_questions_ms", "time_viewing_graded_explanations_ms", "time_viewing_ungraded_explanations_ms", "num_causal_link_adds", "num_causal_link_adds_effective"]]
@@ -96,7 +114,7 @@ def Test_3(df, target):# 92.9%
     predictions = model.predict(X)
     print(model.summary())
 
-def Test_4(df, target): # 94.7% 
+def Test_4(df, target): #   94.7% 
     '''This is the entire data set'''
     X = df
     y = target
@@ -104,7 +122,7 @@ def Test_4(df, target): # 94.7%
     predictions = model.predict(X)
     print(model.summary())
 
-def Test_5(df, target): # 84.8%
+def Test_5(df, target): #   84.8%
     '''This uses the entire data set with sklearn.linear_model.LinearRegression().fit(df,target)'''
     model = linear_model.LinearRegression().fit(df, target)
     predictions = model.predict(df)
@@ -127,11 +145,11 @@ def Test_6(df, target):
         print(type + "\t" + str(confidence))
 
 def RegressionTesting():
-    #load the data
+    #   load the data
     data = LoadData("E:\Downloads")
-    #independent variables
+    #   independent variables
     df = pd.DataFrame(data.data, columns=data.feature_names)
-    #dependent variable
+    #   dependent variable
     target = pd.DataFrame(data.target, columns=["Final_Map_Score"])
 
     Test_1(df, target)
@@ -153,22 +171,25 @@ def RegressionTesting():
 '''*****************************************************************'''
 
 def DecisionTreeTesting():
-    #load the data
+    #   load the data
     data = LoadData1("/home/tdarrah/PythonScripts/")
     df = pd.DataFrame(data.data, columns=data.feature_names)
-    
+
+    #   features
     X = df[["num_map_moves", "time_viewing_concept_map_ms", "num_causal_link_adds", "num_causal_link_adds_effective"]] 
     #X = df[["num_map_moves", "time_viewing_concept_map_ms", "time_viewing_graded_questions_ms", "time_viewing_graded_explanations_ms", "time_viewing_ungraded_explanations_ms", "num_causal_link_adds", "num_causal_link_adds_effective"]]
     #X = df
 
+    #   target
     y = pd.DataFrame(data.target, columns=["subject_id"])
-    #split the data
+    
+    #   split the data
     xTrain, xTest, yTrain, yTest = cross_validation.train_test_split(X, y, test_size = .25, random_state=100)
-    #decision tree classifier
+    
+    #   decision tree classifiers, _1 is gini, _2 is entropy, other parameters can be experimented with 
     dtc_1 = tree.DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=8, min_samples_split=6, min_samples_leaf=3)
     dtc_1.fit(xTrain, yTrain)
     yPred_1 = dtc_1.predict(xTest)
-    print(yPred_1)
     print(metrics.accuracy_score(yTest,yPred_1))
 
     dtc_2 = tree.DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=8, min_samples_split=6, min_samples_leaf=3)
@@ -176,10 +197,11 @@ def DecisionTreeTesting():
     yPred_2 = dtc_2.predict(xTest)
     print(metrics.accuracy_score(yTest,yPred_2))
 
+    #   decision tree visualization, change feature names to match the correct feature set above
     dot_data = tree.export_graphviz(dtc_2, feature_names=["num_map_moves", "time_viewing_concept_map_ms", "num_causal_link_adds", "num_causal_link_adds_effective"], out_file=None, filled=True, rounded=True)
     graph = pydotplus.graph_from_dot_data(dot_data)
    
-    colors = ('turquoise', 'orange', 'blue')
+    colors = ('turquoise', 'orange')
     edges = collections.defaultdict(list)
 
     for edge in graph.get_edge_list():
