7ad19ee5e1a75e3294446b833618de4f167d58e8
darrahts <timothy.s.darrah@vanderbilt.edu>
2018-07-30 08:57:36 -0500

    updated kf

--- kf.py
+++ kf.py
@@ -14,17 +14,13 @@ y_dot(n) = y_dot(n-1) + ay*dt - f*y_dot(n-1)
 since there is no x-acceleration, those terms in x_pos & y_pos will not be included
 in the matricies below
 
-drag is also not included at this time
+drag is pseudo included in the control matrix (as a hack)
 '''
 
 # used to plot ground truth and basis for simulated measurements
 X_true = np.zeros((150, 4))
-
-pred_x = []
-pred_y = []
-
-meas_x = []
-meas_y = []
+X_meas = np.zeros((150,4))
+X_pred = np.zeros((150,4))
 
 k = []
 p = []
@@ -53,7 +49,7 @@ X = np.matrix([[x_pos],
 
 #control matrix
 B = np.matrix([[0],
-               [0],
+               [(1/a)*-1*dt],
                [.5*dt**2],
                [dt]])
 
@@ -73,18 +69,31 @@ P  = np.matrix([[1, .7, 0, 0],
                [.7, 1, 0, 0],
                [0, 0, 1, .7],
                [0, 0, .7, 1]])
+
 #observation matrix
 H = np.eye(4)
 
 #process error covariance matrix
 #Q = lambda: np.eye(4)*random.gauss(5,.5)
-Q = np.eye(4)*5
+Q = np.eye(4)
 
 #measurement error covariance matrix
 R = np.matrix([[36, 0, 0, 0],
                [0, 9, 0, 0],
                [0, 0, 49, 0],
                [0, 0, 0, 16]])
+
+##R = np.matrix([[64, 48, 0, 0],
+##               [48, 64, 0, 0],
+##               [0, 0, 64, 64],
+##               [0, 0, 64, 64]]) 
+
+#               6   6   5   3
+#R = np.matrix([[36, 18, 30, 18],
+#               [18, 9, 15, 9],
+#               [30, 15, 25, 15],
+#               [18, 9, 15, 9]])
+
 
 
 X_hat = X
@@ -101,11 +110,12 @@ X = np.matrix([[10],
                [120],
                [v0*math.sin(math.pi/4)]])
 
+print(X.transpose().shape)
+print(X_pred.shape)
 
 for i in range(0, 150):
     #add predictions to the plot
-    pred_x.append(X[0,0])
-    pred_y.append(X[2,0])
+    X_pred[i] = X.transpose()
 
     #update predictions
     X_hat = A*X + B*U
@@ -113,52 +123,61 @@ for i in range(0, 150):
 
 
     #simulate taking noisy measurements
-    Y[0,0] = random.gauss(X_true[i,0], 12)
-    Y[1,0] = random.gauss(X_true[i,1], 7)
-    Y[2,0] = random.gauss(X_true[i,2], 11)
-    Y[3,0] = random.gauss(X_true[i,3], 13)
+    Y[0,0] = random.gauss(X_true[i,0], 9)
+    Y[1,0] = random.gauss(X_true[i,1], 3)
+    Y[2,0] = random.gauss(X_true[i,2], 9)
+    Y[3,0] = random.gauss(X_true[i,3], 6)
 
     #add measurements to our plots
-    meas_x.append(Y[0,0])
-    meas_y.append(Y[2,0])
-
+    X_meas[i] = Y.transpose()
     #find the residual
     y = Y - H*X_hat
 
     #calculate kalman gain
     S = H*P_hat*np.transpose(H) + R
     K = P_hat*np.transpose(H)*np.linalg.inv(S)
+    #K = np.linalg.solve((P_hat*np.transpose(H)), S)
     k.append(K[0,0])
 
     #update state and covariance
     X = X_hat + K*y
     P = (np.eye(P.shape[0]) - K*H)*P_hat
-    p.append(P[0,0])
-
-    print(k[i], "\t", p[i])
 
     
 plt.figure(1)
-plt.subplot(311)
+plt.subplot(221)
 plt.xlabel("x-pos")
 plt.ylabel("y-pos")
 plt.plot(X_true[:,0],X_true[:,2], '-', label="true pos")
-plt.plot(meas_x, meas_y, '.', label="measured pos")
-plt.plot(pred_x, pred_y, '-', label = "predicted pos")
+plt.plot(X_meas[:,0],X_meas[:,2], ':', label="measured pos")
+plt.plot(X_pred[:,0],X_pred[:,2], '--', label = "predicted pos")
 plt.xlim(xmin=-10)
 plt.ylim(ymin=-10)
 plt.legend()
 
-plt.subplot(312)
+plt.subplot(222)
 plt.xlabel("iterations")
 plt.ylabel("Kalman Gain")
 plt.plot(k, '-')
 
 
-plt.subplot(313)
+plt.subplot(223)
 plt.xlabel("iterations")
-plt.ylabel("covariance")
-plt.plot(p, '-')
+plt.ylabel("y velocity")
+plt.plot(X_true[:,3], '-', label="true")
+plt.plot(X_meas[:,3], ':', label="measured")
+plt.plot(X_pred[:,3], '--', label="predicted")
+plt.legend()
+
+plt.subplot(224)
+plt.xlabel("iterations")
+plt.ylabel("x velocity")
+plt.plot(X_true[:,1], '-', label="true")
+plt.plot(X_meas[:,1], ':', label="measured")
+plt.plot(X_pred[:,1], '--', label="predicted")
+plt.legend()
+
+
 
 plt.subplots_adjust(hspace=.35)
 
