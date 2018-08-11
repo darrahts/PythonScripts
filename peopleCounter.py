7111328d182d1cb3927cae31a490678f65695ab9
darrahts <timothy.s.darrah@vanderbilt.edu>
2018-08-11 10:44:17 -0500

    update people counter

--- peopleCounter.py
+++ peopleCounter.py
@@ -1,21 +1,36 @@
+import numpy as np
 import cv2
 
+cap = cv2.VideoCapture("peopleCounter.avi")
 
-def show_webcam(mirror=False):
-    cam = cv2.VideoCapture(-1)
-    while True:
-        ret_val, img = cam.read()
-        if mirror: 
-            img = cv2.flip(img, 1)
-        cv2.imshow('my webcam', img)
-        if cv2.waitKey(1) == 27: 
-            break  # esc to quit
-    cv2.destroyAllWindows()
+out = cv2.VideoWriter("test.avi", -1, 10.0, (640,480))
 
 
-def main():
-    show_webcam(mirror=True)
+bgSub = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
 
 
-if __name__ == '__main__':
-    main()
+w = cap.get(3)
+h = cap.get(4)
+
+mx = int(w/2)
+my = int(h/2)
+
+count = 0
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    bgMask = bgSub.apply(frame)
+    try:
+        #cv2.putText(frame, "count: {}".format(count), (mx,my), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),1,1)
+        cv2.imshow("Frame", bgMask)
+        count += 1
+    except Exception as e:
+        print(e)
+        print("EOF")
+        break
+
+    if(cv2.waitKey(30) & 0xFF == 27):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
