795083eda53c7accf49e735901f0dd5ef031b6e6
darrahts <timothy.s.darrah@vanderbilt.edu>
2018-08-15 21:44:06 -0500

    added object tracking

---
+++ tracking.py
@@ -0,0 +1,94 @@
+from scipy.spatial import distance as dist
+from collections import OrderedDict, deque, Counter
+import numpy as np
+import pprint
+
+class Trackable():
+    def __init__(self, id, initialLocation, traceLength=20, life=10):
+        self.id = id
+        self.features = dict()
+        self.trace = deque(maxlen=traceLength)
+        self.trace.appendleft(initialLocation)
+        
+        self.features["currentLocation"] = initialLocation
+        self.features["life"] = life
+
+class Tracker():
+    def __init__(self, life=10):
+        self.nextID = 0
+        self.objects = OrderedDict()
+        self.trackLife = life
+
+    def Register(self, object):
+        self.objects[self.nextID] = object
+        self.nextID += 1
+
+    def DeRegister(self, obj):
+        print("deleting: ", end="")
+        print(obj.id)
+        del self.objects[int(obj.id)]
+        
+    def Update(self):
+        print("you need to implement this method!")
+
+
+class CentroidTracker(Tracker):
+    def __init__(self, life=10):
+        Tracker.__init__(self)
+        
+    def Update(self, rects=None):
+        print("overrides base class method.")
+        if(rects is None):
+            for key in list(self.objects.keys()):
+                obj = self.objects.get(key)
+                obj.features["life"] -= 1
+                if(obj.features["life"] == 0):
+                    self.DeRegister(obj)
+                    
+            #return self.objects
+            return
+        
+        centroids = np.zeros((len(rects), 2), dtype="int")
+        
+        for(i, (x0, y0, x1, y1)) in enumerate(rects):
+            cX = int((x0 + x1) / 2.0)
+            cY = int((y0 + y1) / 2.0)
+            centroids[i] = (cX, cY)
+
+
+def Test1():
+    ct = CentroidTracker()
+    ct.Register(Trackable(str(ct.nextID), (0,0)))
+    print(list(ct.objects.keys()))
+    #print(ct.objects["0"].features["life"])
+    ct.Update([(0,0)])
+    ct.Update()
+    ct.Update()
+    ct.Register(Trackable(str(ct.nextID), (1,1)))
+    ct.Update([(0,0)])
+    print(list(ct.objects))
+    ct.Update()
+    ct.Update()
+    print(len(ct.objects.keys()))
+    print(list(ct.objects))
+    for i in range(0, 6):
+        ct.Update()
+        print(len(ct.objects.keys()))
+        print(list(ct.objects))
+    
+    ct.Update()
+    print(len(ct.objects.keys()))
+    print(list(ct.objects))
+    
+    ct.Update()
+    print(len(ct.objects.keys()))
+    print(list(ct.objects))
+
+
+if(__name__ == "__main__"):
+    ct = CentroidTracker()
+    ct.Register(Trackable(ct.nextID, (0,0)))
+    print(ct.objects[0].features["life"])
+    print(list(ct.objects))
+    ct.Update()
+    
